c6148fbdac4646b860ff573c75d7957b19ff8ff9
Hridaya Raj Rajbanshi <35860420+hridayarajbanshi@users.noreply.github.com>
2024-08-03 13:55:31 +0545

    commit

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 
 
 #program to ask 3 numbers and print the greatest number 
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
-c = int(input("Enter the third number: "))
-if(a < b):
-    print("B is greater", b)
-elif(b < c):
-    print("C is greater", c)
-elif(c<a):
-    print("A is greater", a)
+# a = int(input("Enter the first number: "))
+# b = int(input("Enter the second number: "))
+# c = int(input("Enter the third number: "))
+# if(a < b):
+#     print("B is greater", b)
+# elif(b < c):
+#     print("C is greater", c)
+# elif(c<a):
+#     print("A is greater", a)
+
+#while loop
+count = 1
+while (count < 10):
+        print("The count is: ", count)
+        count = count + 1
+    
+# program to ask user with the name of the student and enter the name until selects N or n
+list =[]
+while True:
+    name = input("Enter the name of the student: ")
+    list.append(name)
+    print("Do you want to continue? Y/N")
+    choice = input()
+    if choice == "y":
+        continue
+    else: 
+         print("The list of students are: ", list)
+         break
