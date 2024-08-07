c439b8ca75241348b7c44b5efa05a705bfe8b0c5
Hridaya Raj Rajbanshi <35860420+hridayarajbanshi@users.noreply.github.com>
2024-08-07 12:03:30 +0545

    feat: Add student list page with table display

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(req):
+    return render(req, "appIndex.html")
+def studentList(req):
+    return render(req, "studentList.html")
--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,14 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import indexView, loginPage, registerPage
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', indexView), 
     path("login/", loginPage),
+    path('', include('myapp.urls')),
     path('register/', registerPage)
 
 ]
 
+    
